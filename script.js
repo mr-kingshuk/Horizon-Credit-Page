d7757626c81e4bb7be0a0fb63d72212cfbdfd87b
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-02-17 19:59:13 +0530

    added for multiple sections

--- script.js
+++ script.js
@@ -1,70 +1,77 @@
-const left_arrow = document.querySelector('.left');
-const right_arrow = document.querySelector('.right');
 
-const slider = document.querySelector('.slider');
+const sections = document.querySelectorAll('section');
 
-const indicatorContainer = document.querySelector('.indicators');
-const indicators = document.querySelectorAll('.indicators li');
+sections.forEach(section => {
 
-const totalSlides = slider.childElementCount;
+    const left_arrow = section.querySelector('.left');
+    const right_arrow = section.querySelector('.right');
 
+    const slider = section.querySelector('.slider');
 
+    const indicatorContainer = section.querySelector('.indicators');
+    const indicators = section.querySelectorAll('.indicators li');
 
-var index = 0;
-var numVisSlides = getComputedStyle(document.querySelector(':root')).getPropertyValue('--num-vis-slides');
-// number of times we can swipe left or right
-var swipes = totalSlides - numVisSlides;
+    const totalSlides = slider.childElementCount;
 
 
 
-function indicateGrayArrow() {
-    if (index == 0)
-        left_arrow.classList.add('arrow-grey');
-    else
-        left_arrow.classList.remove('arrow-grey');
-
-    if (index == swipes)
-        right_arrow.classList.add('arrow-grey');
-    else
-        right_arrow.classList.remove('arrow-grey');
-}
-
-function slideOperations() {
-    slider.style.transform = 'translateX(' + (index) * (-(100 / totalSlides)) + "%)";
-
-    document.querySelector('.control .selected').classList.remove('selected');
-
-    indicatorContainer.children[index].classList.add('selected');
-}
+    var index = 0;
+    var numVisSlides = getComputedStyle(document.querySelector(':root')).getPropertyValue('--num-vis-slides');
+    // number of times we can swipe left or right
+    var swipes = totalSlides - numVisSlides;
 
 
-indicators.forEach((indicator, ind) => {
-    indicator.addEventListener('click', () => {
-        index = ind;
-        slideOperations();
+
+
+
+
+    function indicateGrayArrow() {
+        if (index == 0)
+            left_arrow.classList.add('arrow-grey');
+        else
+            left_arrow.classList.remove('arrow-grey');
+
+        if (index == swipes)
+            right_arrow.classList.add('arrow-grey');
+        else
+            right_arrow.classList.remove('arrow-grey');
+    }
+
+    function slideOperations() {
+        slider.style.transform = 'translateX(' + (index) * (-(100 / totalSlides)) + "%)";
+
+        section.querySelector('.control .selected').classList.remove('selected');
+
+        indicatorContainer.children[index].classList.add('selected');
+    }
+
+
+    indicators.forEach((indicator, ind) => {
+        indicator.addEventListener('click', () => {
+            index = ind;
+            slideOperations();
+        });
     });
+
+    right_arrow.addEventListener('click', () => {
+        if (index != swipes) {
+            // changes the index to cureent slide
+            index = (index + 1);
+            indicateGrayArrow();
+            slideOperations();
+        }
+    });
+
+    left_arrow.addEventListener('click', () => {
+        if (index != 0) {
+            // changes the index to current slide
+            index = (index - 1);
+            console.log(index);
+            indicateGrayArrow();
+            slideOperations();
+        }
+    });
+
+
 });
-
-right_arrow.addEventListener('click', () => {
-    if (index != swipes) {
-        // changes the index to cureent slide
-        index = (index + 1);
-        console.log(index);
-        indicateGrayArrow();
-        slideOperations();
-    }
-});
-
-left_arrow.addEventListener('click', () => {
-    if (index != 0) {
-        // changes the index to current slide
-        index = (index - 1);
-        console.log(index);
-        indicateGrayArrow();
-        slideOperations();
-    }
-});
-
-
-
 
